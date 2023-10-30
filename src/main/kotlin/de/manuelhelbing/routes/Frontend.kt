78cdf334570ba03441569bbf0a32e0a92a6f24b8
zoku <mail@zoku.io>
2023-10-30 12:43:02 +0100

    Update

--- src/main/kotlin/de/manuelhelbing/routes/Frontend.kt
+++ src/main/kotlin/de/manuelhelbing/routes/Frontend.kt
@@ -1,16 +1,11 @@
 package de.manuelhelbing.routes
 
-import io.ktor.resources.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.Serializable
-
-@Serializable
-@Resource("/frontend")
-class FrontendRoute
 
 fun Application.frontendRoute() {
     routing {
-        staticResources("/assets", "/static/assets")    }
+        staticResources("/assets", "/static/assets")
+    }
 }
